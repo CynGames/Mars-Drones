ba03146bbb72b6d8d9472806a2082bd9d3f6de35
CynGames <tomasm.leguizamon@gmail.com>
2021-02-02 12:01:27 -0300

    Obj Part 1

---
+++ Assets/Scripts/Minigames/General/Objective.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour
+{
+    [TextArea] public string CurrentObjective_ESP; 
+    [TextArea] public string CurrentObjective_ENG; 
+    [TextArea] public string CurrentObjective_PORT;
+
+    private void Start()
+    {
+        //if español
+        ObjectiveManager.Instance.SetCurrentObjective(CurrentObjective_ESP);
+    }
+}
---
+++ Assets/Scripts/Minigames/ObjectiveManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    public static ObjectiveManager Instance;
+    
+    public TMP_Text ObjBody;
+    
+    [TextArea] public string NoObjective;
+    [TextArea] public string MainMenu;
+    [TextArea] public string PhaseZero;
+    [TextArea] public string PhaseOne;
+    [TextArea] public string PhaseTwo;
+    [TextArea] public string PhaseThree;
+    [TextArea] public string PhaseFour;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+
+    public void SetCurrentObjective(string obj)
+    {
+        ObjBody.text = obj;
+    }
+}
